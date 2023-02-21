3ae0c0dbf8fed20daefcea3a419108264168b606
enapolov <eduard.napolov@toptal.com>
2023-02-21 13:25:00 +0300

    Removed auth module. Added custom auth form

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-    withAuthenticator,
-    Button,
-    Heading,
-    TextField,
-    View,
-    Card,
-    Flex
-} from "@aws-amplify/ui-react";
+import AuthForm from './components/auth/AuthForm.js';
 
-function App({ signOut }) {
+function App() {
     return (
-        <View className="App">
-            <Card variation="elevated">
-                <Heading >Enter user information</Heading>
-                <Flex
-                    direction="column"
-                    justifyContent="flex-end"
-                    alignItems="flex-start"
-                    alignContent="flex-start"
-                    wrap="nowrap"
-                    gap="1rem"
-                >
-                    <TextField
-                        size="default"
-                        label="Last name"
-                        name="lastName"
-                    />
-                    <TextField
-                        size="default"
-                        label="First name"
-                        name="firstName"
-                    />
-                    <TextField
-                        size="default"
-                        label="Email"
-                        name="email"
-                    />
-                    <Button
-                        variation="primary"
-                        size="large"
-                        loadingText=""
-                        onClick={() => alert('hello')}
-                        ariaLabel=""
-                    >
-                        Submit
-                    </Button>
-                </Flex>
-            </Card>
-        </View>
+        <AuthForm />
     );
 }
 
-export default withAuthenticator(App);
+export default App;
---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,53 @@
+import { useState, useRef } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    const [isLogin, setIsLogin] = useState(true);
+
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        if (isLogin) {
+            // do nothing
+        } else {
+            // do login
+        }
+    };
+
+    return (
+        <section className={classes.auth}>
+            <h1>Login</h1>
+            <form onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor='email'>Your Email</label>
+                    <input type='email' id='email' required ref={emailInputRef} />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor='password'>Your Password</label>
+                    <input
+                        type='password'
+                        id='password'
+                        required
+                        ref={passwordInputRef}
+                    />
+                </div>
+                <div className={classes.actions}>
+                    <button>Login</button>
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default AuthForm;
